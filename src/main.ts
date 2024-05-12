7ec50c1b597ffaa3e1825c36cc5a75b6d8b832fb
afovo <12011437@mail.sustech.edu.cn>
2024-05-12 19:55:32 +0800

    b

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import '@/assets/scss/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import AFRAME from 'aframe'
-import router from './router/index'
 import './index.css'
 import { createPinia } from 'pinia'
 const pinia = createPinia()
-createApp(App).use(AFRAME).use(router).use(pinia).mount('#app')
+createApp(App).use(AFRAME).use(pinia).mount('#app')
